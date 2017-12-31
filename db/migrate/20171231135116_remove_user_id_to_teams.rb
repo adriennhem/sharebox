8cd1456bd88c2372e3774a188489c2cbeb8847c9
adriennhem <adrien.nhem@gmail.com>
2017-12-31 14:06:42 +0000

    create relation between user and teams

---
+++ db/migrate/20171231135116_remove_user_id_to_teams.rb
@@ -0,0 +1,5 @@
+class RemoveUserIdToTeams < ActiveRecord::Migration[5.1]
+  def change
+  	remove_column :teams, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171231120820) do
+ActiveRecord::Schema.define(version: 20171231135116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assets", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "team_users", force: :cascade do |t|
+    t.integer "team_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
