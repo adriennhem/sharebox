d32805aaa5e7ab6b2912d6d6c03c9a684da06aa6
adriennhem <adrien.nhem@gmail.com>
2018-01-02 12:58:44 +0000

    update controllers and layout

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,4 @@
-class AssetsController < ApplicationController
+class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   # GET /items
@@ -14,7 +14,7 @@ class AssetsController < ApplicationController
 
   # GET /items/new
   def new
-    @item = Asset.new
+    @item = Item.new
   end
 
   # GET /items/1/edit
@@ -28,7 +28,7 @@ class AssetsController < ApplicationController
 
     respond_to do |format|
       if @item.save
-        format.html { redirect_to @item, notice: 'Asset was successfully created.' }
+        format.html { redirect_to @item, notice: 'Item was successfully created.' }
         format.json { render :show, status: :created, location: @item }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class AssetsController < ApplicationController
   def update
     respond_to do |format|
       if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Asset was successfully updated.' }
+        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
         format.json { render :show, status: :ok, location: @item }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class AssetsController < ApplicationController
   def destroy
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Asset was successfully destroyed.' }
+      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
